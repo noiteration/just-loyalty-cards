47bc60780b546b1753c7d54ec3739eae37ef2bcb
sahirp <47252018+noiteration@users.noreply.github.com>
2026-01-01 21:59:26 -0500

    [feat] Implemented UUID

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,17 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    protected $keyType = 'string';
+    public $incrementing = false;
+    
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasUuids;
 
     /**
      * The attributes that are mass assignable.
